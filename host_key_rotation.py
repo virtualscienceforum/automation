2c2bf64c299b4a7cf53f8f6962b0f6c7e55d4b5c
Anton Akhmerov <anton.akhmerov@gmail.com>
2020-10-07 16:36:14 +0200

    typo fixes

--- host_key_rotation.py
+++ host_key_rotation.py
@@ -17,7 +17,7 @@ def host_key(timeslot: datetime.datetime) -> int:
     """Generate a host key for a specified time."""
     key_salt = os.getenv("HOST_KEY_SALT").encode()
     timestamp = timeslot.replace(second=0, microsecond=0, minute=0).timestamp()
-    hashed = hashlib.sha512(int(timestamp.to_bytes(5, "big")) + key_salt)
+    hashed = hashlib.sha512(int(timestamp).to_bytes(5, "big") + key_salt)
     return f"{int(hashed.hexdigest(), 16) % int(1e7):06}"
 
 
