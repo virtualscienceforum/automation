dccb143a540d71c9404b874fb641bdec060c16be
Anton Akhmerov <anton.akhmerov@gmail.com>
2020-11-23 23:20:41 +0100

    typo fix

--- share_host_key.py
+++ share_host_key.py
@@ -4,7 +4,7 @@ from common import talks_data
 from schedulezoomtalks import notify_author
 
 if __name__ == "__main__":
-    talks = talks_data()
+    talks, _ = talks_data()
     issue_number = int(os.getenv("ISSUE_NUMBER"))
     notify_author(
         next(talk for talk in talks if talk["workflow_issue"] == issue_number)
