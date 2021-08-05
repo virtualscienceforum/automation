92d4fbaa02df15bb9839b53be31315cd7d3c14a3
Evert van Nieuwenburg <evert.v.nieuwenburg@gmail.com>
2021-08-05 21:37:08 +0200

    Update cloudflare worker endpoints

--- cloudflare/index.js
+++ cloudflare/index.js
@@ -1,30 +1,21 @@
 const Router = require('./router')
+const Render = require('./rendertemplates')
 
 const MAILGUN_API_URL = "https://api.eu.mailgun.net/v3"
 const USER = 'api';
 const DOMAIN = 'mail.virtualscienceforum.org'
 const SEND_MAIL_URL = MAILGUN_API_URL + '/' + DOMAIN + '/messages'
 
+const MAILGUNAPIKEY = ""
+const RECAPTCHASITEKEY = ""
+const RECAPTCHASECRET = ""
 const mailGunAuthorization = Buffer.from(USER + ':' + MAILGUNAPIKEY).toString('base64');
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "https://www.virtualscienceforum.org",
-  "Access-Control-Allow-Methods": "GET, HEAD, POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Origin": "*", //https://www.virtualscienceforum.org",
+  "Access-Control-Allow-Methods": "GET, HEAD, POST, OPTIONS, PUT",
+  "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
 }
-
-const registrationConfirmationEmail = `
-  <!DOCTYPE html>
-  <html>
-  <body>
-  <h1>Thank you for registering</h1>
-  <p>Dear NAME,</p>
-  <p>THANKYOUMSG</p>
-  <p>Kind regards,</p>
-  <p>VSF</p>
-  </body>
-  </html>
-  `
 
 const welcomeEmail = `
   <!DOCTYPE html>
@@ -34,7 +25,7 @@ const welcomeEmail = `
   <p>Dear NAME,</p>
   <p>THANKYOUMSG</p>
   <p>Kind regards,</p>
-  <p>VSF</p>
+  <p>The VSF Team</p>
   </body>
   </html>
   `
@@ -70,30 +61,36 @@ function validateMailingListFormData(bodydata)
 
 async function subscribeToMailingList(listName, username, useremail) {
 
-  var addMemberURL = MAILGUN_API_URL + '/lists/' + listName + '@' + DOMAIN + '/members'
+  try {
+    const userdata = {}
+    userdata['name'] = username
+    userdata['address'] = useremail
+    // Toggle subscribed
+    userdata['subscribed'] = true
+    // Update user if present
+    userdata['upsert'] = true
 
-  const userdata = {}
-  userdata['name'] username
-  userdata['address'] = useremail
-  // Toggle subscribed
-  userdata['subscribed'] = true
-  // Update user if present
-  userdata['upsert'] = true
+    // Define the payload
+    let bodyoptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": userdata.length,
+        'Authorization': 'Basic ' + mailGunAuthorization,
+      },
+      body: urlfy(userdata),
+    }
 
-  // Define the payload
-  let bodyoptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "Content-Length": userdata.length,
-      'Authorization': 'Basic ' + mailGunAuthorization,
-    },
-    body: urlfy(userdata),
+    // Send the request
+    var addMemberURL = MAILGUN_API_URL + '/lists/' + listName + '@' + DOMAIN + '/members'
+    const response = await fetch(addMemberURL, bodyoptions);
+    return response
   }
-
-  // Send the request
-  const response = await fetch(addMemberURL, bodyoptions)
-  return response
+  catch (err)
+  {
+    console.error(err)
+    return new Response(err.stack, { status: 500, headers:corsHeaders })
+  }
 }
 
 async function handleMailingListSignupRequest(request) {
@@ -113,7 +110,7 @@ async function handleMailingListSignupRequest(request) {
       }
 
       // Validate the submitted data
-      if (!validateFormData(bodydata)) {
+      if (!validateMailingListFormData(bodydata)) {
         return new Response('Invalid submission', { status: 400, headers:corsHeaders })
       }
 
@@ -147,7 +144,7 @@ async function handleMailingListSignupRequest(request) {
             return new Response("The list " + listsToSubscribeTo[i] + " cannot be subscribed to.", {status:400, headers:corsHeaders})
         }
 
-        const response = subscribeToMailingList(mailgunListName, bodydata['name'], bodydata['address'])
+        const response = await subscribeToMailingList(mailgunListName, bodydata['name'], bodydata['address'])
 
         if( response.status != 200 ) {
           return new Response("Error while signing up for " + listsToSubscribeTo[i], {status:response.status, headers:corsHeaders})
@@ -155,10 +152,11 @@ async function handleMailingListSignupRequest(request) {
       }
 
       // If we get here, we managed to sign up for the lists
-      const sendmailresponse = await sendConfirmationEmail(bodydata.address, bodydata.name, listsToSubscribeTo)
+      const sendmailresponse = await sendMailingListSignupConfirmationEmail(bodydata.address, bodydata.name, listsToSubscribeTo)
       if( sendmailresponse.status != 200 ) {
         return new Response("You were signed up, but sending a confirmation email failed.", {status:sendmailresponse.status, headers:corsHeaders})
       }
+
       return new Response("Succesfully subscribed. You will receive a confirmation email.", {status:200, headers:corsHeaders})
   }
   catch (err)
@@ -225,16 +223,30 @@ async function handleZoomRegistrationRequest(request) {
     }
 
     // Send to Zoom
+    const meetingId = bodydata['meetingID']
     var registerURL = 'https://api.zoom.us/v2/meetings/' + meetingId + '/registrants'
     const response = await fetch(registerURL, requestbody)
 
     // If we get here, we managed to register
-    /*
-    const sendmailresponse = await sendRegistrationConfirmationEmail(bodydata.address, bodydata.name, "LRC")
-    if( sendmailresponse.status != 200 ) {
-      return new Response("You succesfully registered, but sending the confirmation email failed.", {status:sendmailresponse.status, headers:corsHeaders})
+
+    // Add to mailing list!
+    const username = bodydata["firstname"] + " " + bodydata["lastname"]
+    const useremail = bodydata["address"]
+    const eventtype = bodydate["eventType"]
+
+    var mailgunListName = "";
+    switch( eventtype ) {
+      case "lrc":
+        mailgunListName = "vsf-announce"
+        break;
+      case "speakers_corner":
+        mailgunListName = "speakers_corner"
+        break;
+      default:
+        return new Response("The list " + eventtype + " cannot be subscribed to.", {status:400, headers:corsHeaders})
     }
-    */
+    const result = subscribeToMailingList(mailgunListName, username, useremail)
+
     return new Response("Succesfully registered. You will receive a confirmation email.", {status:200, headers:corsHeaders})
   }
   catch (err)
@@ -244,50 +256,17 @@ async function handleZoomRegistrationRequest(request) {
   }
 }
 
-async function sendRegistrationConfirmationEmail(address, name, talk) {
-
-  // Update the template
-  var thankYouMsg = "Thank you for registering for %s"%talk
-  var mailBody = registrationConfirmationEmail.replace("NAME", name)
-  var mailBody = mailBody.replace("THANKYOUMSG", thankYouMsg)
-
-  let bodydata = {
-    from: "mail@virtualscienceforum.org",
-    to: address,
-    subject: "Thank you for registering for the Virtual Sciende Forum LRC",
-    html: mailBody,
-  }
-
-  let bodyoptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      "Content-Length": bodydata.length,
-      'Authorization': 'Basic ' + mailGunAuthorization,
-    },
-    body: urlfy(bodydata),
-  }
-
-  const response = await fetch(SEND_MAIL_URL, bodyoptions)
-  if( response.status != 200 ) {
-    return new Response("Error while sending the confirmation email", {status:response.status, headers:corsHeaders})
-  }
-  return new Response("results", {status:200, headers:corsHeaders})
-}
-
-
 function getListName(list)
 {
   switch( list ) {
     case "signup-general":
       return "General announcement mailing list"
     case "signup-speakerscorner":
-      return "Speaker\'s corner mailing list"
+      return "Speakers\' corner mailing list"
   }
 }
 
 async function sendMailingListSignupConfirmationEmail(address, name, lists) {
-
     // Update the template
     var thankYouMsg = "Thank you for signing up for ";
     for( var i = 0; i < lists.length; i++ ) {
@@ -325,48 +304,49 @@ async function sendMailingListSignupConfirmationEmail(address, name, lists) {
     return response
 }
 
-function respondWithRawHTML(html) {
-  const init = {
-    headers: {
-      "content-type": "text/html;charset=UTF-8",
-    },
+function handleOptions(request) {
+  // Make sure the necessary headers are present
+  // for this to be a valid pre-flight request
+  if(
+    request.headers.get("Origin") !== null &&
+    request.headers.get("Access-Control-Request-Method") !== null &&
+    request.headers.get("Access-Control-Request-Headers") !== null
+  ){
+    // Handle CORS pre-flight request.
+    // If you want to check the requested method + headers
+    // you can do that here.
+    return new Response(null, {
+      headers: corsHeaders,
+    })
   }
-  return new Response(html, init)
-}
-
-
-async function renderRegistrationForm(request) {
-
-  const formData = await request.formData()
-  const bodydata = {}
-  for (const entry of formData.entries()) {
-    bodydata[entry[0]] = entry[1]
+  else {
+    // Handle standard OPTIONS request.
+    // If you want to allow other HTTP Methods, you can do that here.
+    return new Response(null, {
+      headers: {
+        Allow: "GET, HEAD, POST, OPTIONS",
+      },
+    })
   }
-
-  var htmlForm = registrationForm.replace("SPEAKERNAME", bodydata['speakerName'])
-  var htmlForm = htmlForm.replace("MEETINGID", bodydata['meetingID'])
-
-  const init = {
-    headers: {
-      "content-type": "text/html;charset=UTF-8",
-    },
-  }
-  return new Response(htmlForm, init)
 }
 
 addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
+    if (event.request.method === "OPTIONS") {
+        // Handle CORS preflight requests
+        event.respondWith(handleOptions(request))
+    } else {
+      event.respondWith(handleRequest(event.request))
+    }
 })
 
-
 async function handleRequest(request) {
-    // Replace with the appropriate paths and handlers
     const r = new Router()
 
     r.post('/register', request => handleZoomRegistrationRequest(request))
     r.post('/mailinglist', request => handleMailingListSignupRequest(request) )
 
-    r.get('/', () => new Response('Hello from our VSF worker! Please visit /LRC')) // return a default message for the root route
+    // return a default message for the root route
+    r.get('/', () => new Response('Hello from our VSF worker!'))
 
     const resp = await r.route(request)
     return resp
